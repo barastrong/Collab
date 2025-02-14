1c78415925d32de6fab81491c702c335573c3f56
barastrong <145325312+barastrong@users.noreply.github.com>
2025-02-14 09:10:39 +0700

    Collab

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Barang;
 
 class BarangController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Barang::all();
+        return view('admin.barang.index', compact('data'));
+    }
 }
